8df723e02b8cd39cb1a614b48c39b35cb8cba557
hobbyluv <princewillchime43@gmail.com>
2020-03-27 02:14:07 +0100

    building a shopping cart for body wears and have add the structure of the collection for the cart

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return <div className="App">welcome to my shopping cart</div>;
+}
+
+export default App;
---
+++ src/Collection.component.js/Categories.carts.js
@@ -0,0 +1,50 @@
+const Categories = [];
+
+(Categories["canvas"] = {
+  products: [
+    {
+      name: "canvas",
+      amount: "$" + 2000,
+      image:
+        "https://res.cloudinary.com/hobbyluv07/image/upload/v1568417010/52675_NVOR_zutngd.jpg"
+    },
+    {
+      name: "sanchos",
+      amount: "$" + 5000,
+      image:
+        "https://res.cloudinary.com/hobbyluv07/image/upload/v1568417052/Mens_Boat_Shoes._CB442712240__c9bu6u.jpg"
+    }
+  ]
+}),
+  (Categories["sanchos"] = {
+    products: [
+      {
+        name: "sanchos",
+        amount: "$" + 5000,
+        image:
+          "https://res.cloudinary.com/hobbyluv07/image/upload/v1568417052/Mens_Boat_Shoes._CB442712240__c9bu6u.jpg"
+      }
+    ]
+  }),
+  (Categories["hills shoes"] = {
+    products: [
+      {
+        name: "hills-shoes",
+        amount: "$" + 3000,
+        image:
+          "https://res.cloudinary.com/hobbyluv07/image/upload/v1568416986/81V4ktn_z_L._Ac_SX255__hxdsxt.jpg"
+      }
+    ]
+  }),
+  (Categories["flat cruros"] = {
+    products: [
+      {
+        name: "flat-cruros",
+        amount: "$" + 2000,
+        image:
+          "https://res.cloudinary.com/hobbyluv07/image/upload/v1568417052/Mens_Boat_Shoes._CB442712240__c9bu6u.jpg"
+      }
+    ]
+  });
+
+export default Categories;
