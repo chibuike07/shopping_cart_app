0e3418532dcecf5810d26cde203eda15931db858
hobbyluv <princewillchime43@gmail.com>
2020-04-06 18:38:04 +0100

    fixed the protecting of multiple component from unregistered members

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-// import Dashboard from "./AuthUserRoutes/View/Pages";
 import SignUp from "./Views.component/Registration.views.component/SignUp.registration";
 import Login from "./Views.component/Registration.views.component/Login.registration";
 import SignInReg from "./Views.component/Registration.views.component/Signin.reg";
-// import ProductDisplay from "./Views.component/Products.display.views.component/products.product.display";
 import PageForLoggedUsers from "./AuthUserRoutes/View/Pages";
 function App() {
   const Display = () => <h2>Sorry this page does not exist</h2>;
@@ -16,8 +14,8 @@ function App() {
           <Route exact path="/" component={SignInReg} />
           <Route exact path="/signup" component={SignUp} />
           <Route exact path="/login" component={Login} />
-          <Route exact path="/dashboard" component={PageForLoggedUsers} />
-          {/* {PageForLoggedUsers} */}
+          {/* <Route exact path="/dashboard" component={PageForLoggedUsers} /> */}
+          <PageForLoggedUsers />
           {/* <Route exact path="/categories" component={ProductDisplay} /> */}
           <Route component={Display} />
         </Switch>
